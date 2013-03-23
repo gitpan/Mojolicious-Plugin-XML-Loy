f2dc7eb072512fb87dfe54d94474c13f1d297d52
Nils Diewald <akron@cpan.org>
2013-03-23 21:51:06 +0000

    Import of AKRON/Mojolicious-Plugin-XML-Loy-0.01 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-XML-Loy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AKRON/Mojolicious-Plugin-XML-Loy-0.01.tar.gz
    gitpan-cpan-author:       AKRON
    gitpan-cpan-maturity:     released

---
+++ lib/Mojolicious/Plugin/XML/Loy.pm
@@ -0,0 +1,294 @@
+package Mojolicious::Plugin::XML::Loy;
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Loader;
+use XML::Loy;
+
+our $VERSION = '0.01';
+
+my %base_classes;
+
+# Register Plugin
+sub register {
+  my ($plugin, $mojo, $param) = @_;
+
+  my $namespace = 'XML::Loy::';
+  my $max_size = 1024 * 1024;
+
+  # Load parameter from Config file
+  if (my $config_param = $mojo->config('XML-Loy')) {
+    $param = { %$config_param, %$param };
+  };
+
+  # Set Namespace
+  if (exists $param->{namespace}) {
+    $namespace = delete $param->{namespace};
+    $namespace .= '::' if $namespace;
+  };
+
+  if (exists $param->{max_size} && $param->{max_size} =~ /^\d+$/) {
+    $max_size = delete $param->{max_size};
+  };
+
+  # Start Mojo::Loader instance
+  my $loader = Mojo::Loader->new;
+
+  # Create new XML helpers
+  foreach my $helper (keys %$param) {
+    my @helper = @{ $param->{ $helper } };
+    my $base = shift @helper;
+
+    $base = ($base eq 'Loy' ? 'XML::Loy' : $namespace . "$base");
+
+    # Load module if not loaded
+    unless (exists $base_classes{$base}) {
+
+      # Load base class
+      if (my $e = $loader->load($base)) {
+	for ($mojo->log) {
+	  $_->error("Exception: $e")  if ref $e;
+	  $_->error(qq{Unable to load base class "$base"});
+	};
+	next;
+      };
+
+      my $mime   = $base->mime;
+      my $prefix = $base->_prefix;
+
+      # Establish mime types
+      if ($mime && $prefix) {
+
+	# Apply mime type
+	$mojo->types->type($prefix => $mime);
+      };
+
+      # module loaded
+      $base_classes{$base} = [$prefix => $mime];
+    };
+
+    # Code generation for ad-hoc helper
+    my $code = 'sub { shift;' .
+      ' { use bytes; return if length("@_") > ' . $max_size . '} ' .
+      ' my $doc = ' . $base . '->new( @_ );';
+
+    # Extend base class
+    if (@helper) {
+      $code .= '$doc->extension(' .
+	join(',', map( '"' . $namespace . qq{$_"}, @helper)) .
+      ");";
+    };
+    $code .= 'return $doc };';
+
+    # Eval code
+    my $code_ref = eval $code;
+
+    # Evaluation error
+    $mojo->log->fatal($@ . ': ' . $!) and next if $@;
+
+    # Create helper
+    $mojo->helper($helper => $code_ref);
+  };
+
+  # Plugin wasn't registered before
+  unless (exists $mojo->renderer->helpers->{'new_xml'}) {
+
+    # Default 'new_xml' helper
+    $mojo->helper(
+      new_xml => sub {
+	shift;
+	return XML::Loy->new( @_ );
+      });
+
+
+    # Add 'render_xml' helper
+    $mojo->helper(
+      render_xml => sub {
+	my ($c, $xml) = @_;
+	my $format = 'xml';
+
+	# Check format based on mime type
+	my $class = ref $xml;
+	if ($base_classes{$class}) {
+	  if ($base_classes{$class}->[0] && $base_classes{$class}->[1]) {
+	    $format = $base_classes{$class}->[0];
+	  };
+	};
+
+	# render XML with correct mime type
+	return $c->render_data(
+	  $xml->to_pretty_xml,
+	  'format' => $format,
+	  @_
+	);
+      });
+  };
+};
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Mojolicious::Plugin::XML::Loy - XML generation with Mojolicious
+
+
+=head1 SYNOPSIS
+
+  # Mojolicious
+  $mojo->plugin(
+    'XML::Loy' => {
+      new_activity => ['Atom', 'ActivityStreams'],
+      new_hostmeta => ['XRD', 'HostMeta'],
+      new_myXML    => ['Loy', 'Atom', 'Atom-Threading']
+    });
+
+  # In controllers use the generic new_xml helper
+  my $xml = $c->new_xml('entry');
+
+  # Create a new XML::Loy document
+  my $env = $xml->add('fun:env' => { foo => 'bar' });
+  $xml->namespace(fun => 'http://sojolicio.us/ns/fun');
+  my $data = $env->add(data => {
+    type  => 'text/plain',
+    -type => 'armour:30'
+  } => <<'B64');
+    VGhpcyBpcyBqdXN0IGEgdGVzdCBzdHJpbmcgZm
+    9yIHRoZSBhcm1vdXIgdHlwZS4gSXQncyBwcmV0
+    dHkgbG9uZyBmb3IgZXhhbXBsZSBpc3N1ZXMu
+  B64
+  $data->comment('This is base64 data!');
+
+  # Render document with the correct mime-type
+  $c->render_xml($xml);
+
+  # Content-Type: application/xml
+  # <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  # <entry xmlns:fun="http://sojolicio.us/ns/fun">
+  #   <fun:env foo="bar">
+  #
+  #     <!-- This is base64 data! -->
+  #     <data type="text/plain">
+  #       VGhpcyBpcyBqdXN0IGEgdGVzdCBzdH
+  #       JpbmcgZm9yIHRoZSBhcm1vdXIgdHlw
+  #       ZS4gSXQncyBwcmV0dHkgbG9uZyBmb3
+  #       IgZXhhbXBsZSBpc3N1ZXMu
+  #     </data>
+  #   </fun:env>
+  # </entry>
+
+  # Use newly created helper
+  my $xrd = $c->new_hostmeta;
+
+  # Create a document based on the defined xml profile
+  $xrd->host('sojolicio.us');
+
+  # Render document with the correct mime-type
+  $c->render_xml($xrd);
+
+  # Content-Type: application/xrd+xml
+  # <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  # <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"
+  #      xmlns:hm="http://host-meta.net/xrd/1.0"
+  #      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  #   <hm:Host>sojolicio.us</hm:Host>
+  # </XRD>
+
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::XML::Loy> is a plugin to support
+XML document generation based on L<XML::Loy>.
+
+
+=head1 METHODS
+
+L<Mojolicious::Plugin::XML::Loy> inherits all methods
+from L<Mojolicious::Plugin> and implements the following
+new ones.
+
+=head2 register
+
+  # Mojolicious
+  $mojo->plugin('XML::Loy' => {
+    max_size     => 2048,
+    namespace    => 'MyOwn::XML',
+    new_activity => ['Atom', 'ActivityStreams']
+  });
+
+  # Mojolicious::Lite
+  plugin 'XML::Loy' => {
+    new_activity => ['Atom', 'ActivityStreams']
+  };
+
+  # In your config file
+  {
+    'XML-Loy' => {
+      new_activity => ['Atom', 'ActivityStreams']
+    }
+  };
+
+Called when registering the plugin.
+Accepts new xml profiles, defined by the
+name of the associated generation helper
+and an array reference defining the profile.
+The first element in the array is the base class,
+followed by all extensions.
+To create a helper extending the base class,
+use C<Loy> as the first element.
+
+  $mojo->plugin('XML::Loy' => {
+    new_myXML => ['Loy', 'Atom']
+  });
+
+In addition to that, the C<max_size> in bytes of xml documents
+to be parsed can be defined (defaults to C<1024 * 1024>) and the
+namespace of all L<XML::Loy> extensions, defaults to C<XML::Loy>.
+
+All parameters can be set either on registration or
+as part of the configuration file with the key C<XML-Loy>.
+
+
+=head1 HELPERS
+
+=head2 new_xml
+
+  my $xml = $c->new_xml('entry');
+  print $xml->to_pretty_xml;
+
+Creates a new generic L<XML::Loy> document.
+All helpers created on registration accept
+the parameters as defined in the constructors of
+the L<XML::Loy> base classes.
+
+
+=head2 render_xml
+
+  $c->render_xml($xml);
+  $c->render_xml($xml, status => 404);
+
+Renders documents based on L<XML::Loy>
+using the defined mime-type of the base class.
+
+
+=head1 DEPENDENCIES
+
+L<Mojolicious>,
+L<XML::Loy>.
+
+=head1 AVAILABILITY
+
+  https://github.com/Akron/Mojolicious-Plugin-XML-Loy
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011-2013, L<Nils Diewald|http://nils-diewald.de/>.
+
+This program is free software, you can redistribute it
+and/or modify it under the same terms as Perl.
+
+=cut
---
+++ t/XML-Loy.t
@@ -0,0 +1,229 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+$|++;
+
+use lib ('lib', '../lib', '../../lib', '../../../lib');
+
+use Mojo::ByteStream 'b';
+use Test::Mojo;
+use Mojolicious::Lite;
+
+use Test::More;
+use Test::Warn;
+
+my $poco_ns  = 'http://www.w3.org/TR/2011/WD-contacts-api-20110616/';
+my $xhtml_ns = 'http://www.w3.org/1999/xhtml';
+
+use_ok('Mojolicious::Plugin::XML::Loy');
+
+# Plugin helper
+my $t = Test::Mojo->new;
+my $app = $t->app;
+
+$app->log->level('error');
+
+ok($app->plugin('XML::Loy' => {
+  new_atom => ['Atom'],
+  new_atom_threading => ['Atom','Atom::Threading'],
+  new_myxml => ['Loy', 'Atom'],
+  new_hostmeta => ['XRD', 'HostMeta'],
+  max_size => 700
+}), 'New plugin');
+
+ok(my $atom = $app->new_atom('feed'), 'Create new feed');
+
+ok(my $atom_string = $atom->to_pretty_xml, 'Create pretty xml');
+$atom_string =~ s/[\s\r\n]+//g;
+
+is ($atom_string, '<?xmlversion="1.0"encoding="UTF-8'.
+                  '"standalone="yes"?><feedxmlns="ht'.
+                  'tp://www.w3.org/2005/Atom"/>',
+                  'Initial Atom');
+
+ok(my $entry = $atom->entry(id => '#33775'), 'Add entry');
+
+ok(my $person = $atom->new_person(
+  name => 'Bender',
+  uri => 'http://sojolicio.us/bender'
+), 'Add person');
+
+$person->namespace('poco' => $poco_ns);
+$person->add('uri', 'http://sojolicio.us/fry');
+$person->add('poco:birthday' => '1/1/1970');
+
+
+ok($entry->author($person), 'Add author');
+
+is($atom->at('author name')->text, 'Bender', 'Author-Name');
+is($atom->at('author uri')->text, 'http://sojolicio.us/bender', 'Author-URI');
+is($atom->at('author birthday')->text, '1/1/1970', 'Author-Poco-Birthday');
+is($atom->at('author birthday')->namespace, $poco_ns, 'Author-Poco-NS');
+
+ok($atom->title(
+  type => 'html',
+  content => 'Dies ist <b>html</b> Inhalt.'
+), 'Add title');
+
+ok($atom->content(
+  type => 'xhtml',
+  content => 'This is <b>xhtml</b> content!'
+), 'Add content');
+
+ok(my $atom2 = $app->new_atom($atom->to_pretty_xml), 'Parse xml');
+
+is($atom2->at('content div b')->text, 'xhtml', 'Pretty Print');
+
+warning_is { $atom->in_reply_to('http://sojolicio.us') } q{Can't locate "in_reply_to" in "XML::Loy::Atom"}, "No threading";
+
+
+# Test Threading
+ok($entry = $app->new_atom_threading('entry'), 'New atom document');
+
+is($entry->at(':root')->namespace, 'http://www.w3.org/2005/Atom', 'Namespace');
+
+ok($person = $entry->new_person(name => 'Zoidberg'), 'New person');
+
+ok($entry->author($person), 'Add author');
+
+is($entry->at('entry > author > name')->text, 'Zoidberg', 'Name');
+
+ok($entry->contributor($person), 'Contributor');
+
+is($entry->at('entry > contributor > name')->text, 'Zoidberg', 'Name');
+
+$entry->id('http://sojolicio.us/blog/2');
+
+is($entry->at('entry')->attrs->{'xml:id'}, 'http://sojolicio.us/blog/2', 'id');
+is($entry->at('entry id')->text, 'http://sojolicio.us/blog/2', 'id');
+
+ok($entry->replies('http://sojolicio.us/entry/1/replies' => {
+  count => 5,
+  updated => '500000'
+}), 'Add replies entry');
+
+ok(my $link = $entry->at('link[rel="replies"]'), 'Get replies link');
+is($link->attrs('thr:count'), 5, 'Thread Count');
+is($link->attrs('thr:updated'), '1970-01-06T18:53:20Z', 'Thread update');
+is($link->attrs('href'), 'http://sojolicio.us/entry/1/replies', 'Thread href');
+is($link->attrs('type'), 'application/atom+xml', 'Thread type');
+is($link->namespace, 'http://www.w3.org/2005/Atom', 'Thread namespace');
+
+ok($entry->total(8), 'Set total');
+
+is($entry->at('total')->text, 8, 'Total number');
+is($entry->at('total')->namespace,
+   'http://purl.org/syndication/thread/1.0',
+   'Total namespace'
+ );
+
+ok($entry->in_reply_to(
+  'http://sojolicio.us/blog/1' => {
+    href => 'http://sojolicio.us/blog/1x'
+  }), 'Add in-reply-to');
+
+is($entry->at('in-reply-to')->namespace,
+   'http://purl.org/syndication/thread/1.0', 'In-reply-to namespace');
+
+is($entry->at('in-reply-to')->attrs('href'),
+   'http://sojolicio.us/blog/1x', 'In-reply-to href');
+
+is($entry->at('in-reply-to')->attrs('ref'),
+   'http://sojolicio.us/blog/1', 'In-reply-to ref');
+
+ok(my $atom3 = $app->new_myxml('test'), 'New object');
+is($atom3->mime, 'application/xml', 'Mime');
+ok(my $myelem = $atom3->add('myelem'), 'New element');
+ok($myelem->author(name => 'Mario'), 'Add author name');
+
+is($myelem->mime, 'application/xml', 'Mime');
+
+is($myelem->type, 'myelem', 'Mime');
+is($myelem->namespace, '', 'Mime');
+is($myelem->at('name')->namespace, 'http://www.w3.org/2005/Atom', 'Mime');
+
+
+# Render
+get '/' => sub {
+  my $c = shift;
+  my $entry = $c->new_atom_threading('entry');
+  $entry->author(name => 'Akron');
+  return $c->render_xml($entry);
+};
+
+get '/xml' => sub {
+  my $c = shift;
+  my $entry = $c->new_xml(test => { root => 'yes' } => 'Works!');
+  return $c->render_xml($entry);
+};
+
+get '/fail' => sub {
+  my $c = shift;
+  my $entry = $c->new_xml(test => { root => 'yes' } => 'Works!');
+  return $c->render_xml($entry, status => 400);
+};
+
+$t->get_ok('/')
+  ->content_like(qr{<author>})
+  ->content_like(qr{<name>Akron</name>})
+  ->content_type_is('application/atom+xml')
+  ->status_is(200);
+
+$t->get_ok('/xml')
+  ->content_like(qr{<test})
+  ->content_like(qr{root="yes"})
+  ->content_type_is('application/xml')
+  ->status_is(200);
+
+$t->get_ok('/fail')
+  ->content_like(qr{<test})
+  ->content_like(qr{root="yes"})
+  ->text_is(':root', 'Works!')
+  ->content_type_is('application/xml')
+  ->status_is(400);
+
+get '/utf-8' => sub {
+  my $c = shift;
+  my $entry = $c->new_xml(test => { root => 'yes' } => 'Wörks!');
+  $entry->add('yeah' => 'üöä');
+  return $c->render_xml($entry, status => 400);
+};
+
+$t->get_ok('/utf-8')
+  ->content_like(qr{<test})
+  ->content_like(qr{root="yes"})
+  ->content_type_is('application/xml')
+  ->text_is(':root', 'Wörks!')
+  ->text_is('yeah', 'üöä')
+  ->status_is(400);
+
+
+# Max size
+ok($atom->add('Further' => 'This expands it')->add('Even' => 'further'),
+   'Extend object');
+ok(!($atom2 = $app->new_atom($atom->to_pretty_xml)), 'Parse xml');
+ok(!$atom2, 'To big');
+
+
+get '/hostmeta' => sub {
+  my $c = shift;
+  my $xrd = $c->new_hostmeta;
+  $xrd->host('sojolicio.us');
+
+  # Render document with the correct mime-type
+  return $c->render_xml($xrd);
+};
+
+$t->get_ok('/hostmeta')
+  ->content_like(qr{<hm:Host})
+  ->content_like(qr{xmlns:xsi})
+  ->content_like(qr{xmlns:hm})
+  ->content_type_is('application/xrd+xml')
+  ->text_is('Host', 'sojolicio.us')
+  ->status_is(200);
+
+
+done_testing;
+
+__END__
